de4f87be24749df4b83fb80f6da7fc6112752399
Juan Lopes <juan.lopes@zwift.com>
2021-12-06 02:44:49 -0300

    Start renaming

--- day6b.py
+++
@@ -1,5 +0,0 @@
-from collections import Counter
-C = Counter(map(int, input().split(',')))
-for day in range(256):
-    C = {i: C.get(i+1, 0) + C.get(0, 0) * (i in (6, 8)) for i in range(9)}
-print(sum(C.values()))
