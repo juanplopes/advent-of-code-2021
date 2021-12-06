b236fc447d7f4efd6e9795466b75819f0ca907c9
Juan Lopes <juan.lopes@zwift.com>
2021-12-06 02:42:27 -0300

    Day 6 in Python

--- day6b.py
+++ day6b.py
@@ -1,6 +1,5 @@
 from collections import Counter
 C = Counter(list(map(int, input().split(','))))
 for day in range(256):
-    C = (Counter({i-1: C.get(i, 0) for i in range(1, 9)}) 
-        + Counter({6: C.get(0, 0), 8: C.get(0, 0)})) 
+    C = {i: C.get(i+1, 0) + C.get(0, 0) * (i in (6, 8)) for i in range(9)}
 print(sum(C.values()))
