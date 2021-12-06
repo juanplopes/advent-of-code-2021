c53a78d4b716f31738a023202c322cfc2105aa3c
Juan Lopes <juan.lopes@zwift.com>
2021-12-06 02:35:25 -0300

    Fix variable name

--- day6b.py
+++ day6b.py
@@ -1,6 +1,6 @@
 from collections import Counter
 C = Counter(list(map(int, input().split(','))))
-for i in range(256):
+for day in range(256):
     C = (Counter({i-1: C.get(i, 0) for i in range(1, 9)}) 
         + Counter({6: C.get(0, 0), 8: C.get(0, 0)})) 
 print(sum(C.values()))
