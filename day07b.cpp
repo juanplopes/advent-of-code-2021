a91b1e195ee56a2dd1b5e17f2f88cdcf999edb72
Juan Lopes <juan.lopes@zwift.com>
2021-12-07 10:55:26 -0300

    Removing ternary search

--- day07b.cpp
+++ day07b.cpp
@@ -3,39 +3,23 @@
 using namespace std;
 
 int T[10000];
-int N = 0;
-
-int f(int x) {
-    int answer = 0;
-    for(int i=0; i<N; i++) {
-        int value = abs(T[i] - x);
-        answer += (value*value+value)/2;
-    }
-    return answer;
-}
-
-int ternary(int left, int right) {
-    while(left < right) {
-        int third1 = left + (right - left) / 3;
-        int third2 = right - (right - left) / 3;
-        int f1 = f(third1), f2 = f(third2);
-        if (f1 > f2) {
-            left = third2 + 1;
-        } else if (f1 < f2) {
-            right = third1 - 1;
-        } else {
-            return f1;
-        }
-    }
-    return f(left);
-}
 
 int main() {
-    char comma;
+    int N = 0; char comma;
     while(cin >> T[N]) {
         cin >> comma;
         N++;
     }
     sort(T, T+N);
-    cout << ternary(T[0], T[N-1]) << endl;
+
+    long long answer = 0x7f7f7f7f;
+    for(int j=T[0]; j<=T[N-1]; j++) {
+        long long current = 0;
+        for(int i=0; i<N; i++) {
+            long long x = abs(T[i] - j);
+            current += x*(x+1)/2;
+        }
+        answer = min(answer, current);
+    }
+    cout << answer << endl;
 }
