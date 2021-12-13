5242793e1ff85a5b76e4281c6f54bdcf6a484bb5
Juan Lopes <juan.lopes@zwift.com>
2021-12-12 23:59:34 -0300

    Use @cache with frozenset

--- day12b.py
+++ day12b.py
@@ -3,16 +3,16 @@ from functools import cache
 G = defaultdict(lambda: [])
 
 @cache
-def count(src, dst, twice, force, visited = frozenset()):
-    if not force and src in visited: 
+def count(src, dst, twice, visited = frozenset()):
+    if src in visited: 
         if twice and src not in ('start', 'end'):
-            return count(src, dst, False, True, visited)
+            return count(src, dst, False, visited - {src})
         else:
             return 0
 
     if src == dst: return 1
     if 'a' <= src[0] <= 'z': visited |= {src}
-    return sum(count(x, dst, twice, False, visited) for x in G[src])
+    return sum(count(x, dst, twice, visited) for x in G[src])
 
 while True:
     try: a, b = input().split('-')
@@ -20,4 +20,4 @@ while True:
     G[a].append(b)
     G[b].append(a)
 
-print(count('start', 'end', True, False))
+print(count('start', 'end', True))
