e86a286bbfdbde7d049aa8da7bd27f4ccacad239
Juan Lopes <juan.lopes@zwift.com>
2021-12-13 08:15:32 -0300

    Day 13

---
+++ day13a.py
@@ -0,0 +1,20 @@
+def fold(T, over_x, value):
+    for x, y in T:
+        yield (2*value - x if over_x and x > value else x, 
+               2*value - y if not over_x and y > value else y)
+
+T = set()
+while True:
+    line = input()
+    if line == '': break
+    T.add(tuple(map(int, line.split(','))))
+
+while True:
+    try: a, b = input().split(' ')[2].split('=')
+    except EOFError: break
+    b = int(b)
+    
+    T = set(fold(T, a == 'x', b))
+    break
+
+print(len(T))
---
+++ day13b.py
@@ -0,0 +1,23 @@
+def fold(T, over_x, value):
+    for x, y in T:
+        yield (2*value - x if over_x and x > value else x, 
+               2*value - y if not over_x and y > value else y)
+
+def display(T):
+    for y in range(10):
+        print(''.join('X' if (x, y) in T else '.' for x in range(50)))
+
+T = set()
+while True:
+    line = input()
+    if line == '': break
+    T.add(tuple(map(int, line.split(','))))
+
+while True:
+    try: a, b = input().split(' ')[2].split('=')
+    except EOFError: break
+    b = int(b)
+    
+    T = set(fold(T, a == 'x', b))
+    
+display(T)
