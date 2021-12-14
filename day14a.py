b4e4347a4debcfcebf80ad6faf56f94cd143cf5e
Juan Lopes <juan.lopes@zwift.com>
2021-12-14 09:21:40 -0300

    Day 14

---
+++ day14a.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+start = input()
+input()
+rules = {}
+while True:
+    try: a, b = input().split(' -> ')
+    except EOFError: break
+    rules[tuple(a)] = b
+
+state = Counter(zip(start, start[1:]))
+for i in range(10):
+    new_state = Counter()
+    for (a, b), value in state.items():
+        c = rules[(a, b)]
+        new_state[(a, c)] += value
+        new_state[(c, b)] += value
+    state = new_state
+
+answer = Counter((start[0], start[-1]))
+for (a, b), value in state.items():
+    answer[a] += value
+    answer[b] += value
+
+print(max(answer.values())//2 - min(answer.values())//2)
