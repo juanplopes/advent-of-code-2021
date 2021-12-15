32d9e87e95e287292e9c3ca2b105198d50d32a3f
Juan Lopes <juan.lopes@zwift.com>
2021-12-15 09:05:59 -0300

    Simplify

--- day15a.py
+++ day15a.py
@@ -1,25 +1,19 @@
 import heapq
 
-def add(G, heap, best, distance, x, y):
-    if x < 0 or y < 0 or x >= len(G) or y >= len(G): return
-    if distance + G[x][y] >= best[x][y]: return
-    heapq.heappush(heap, (distance + G[x][y], (x, y)))
-
 def dijkstra(G, start_node, end_node):
     best = [[float('+Inf')]*len(G) for _ in range(len(G))]
-    
     heap = []
+
     heapq.heappush(heap, (0, start_node))
     while len(heap):
         distance, (x, y) = heapq.heappop(heap)
-        if distance >= best[x][y]: continue
         if (x, y) == end_node: return distance
-        best[x][y] = distance
-        
-        add(G, heap, best, distance, x-1, y)
-        add(G, heap, best, distance, x+1, y)
-        add(G, heap, best, distance, x, y-1)
-        add(G, heap, best, distance, x, y+1)
+        for nx, ny in (x-1, y), (x+1, y), (x, y-1), (x, y+1):
+            if not (0 <= nx < len(G) and 0 <= ny < len(G)): continue
+            ndistance = distance + G[nx][ny]
+            if ndistance >= best[nx][ny]: continue
+            best[nx][ny] = ndistance
+            heapq.heappush(heap, (ndistance, (nx,ny)))
 
 G = []
 while True:
--- day15b.py
+++ day15b.py
@@ -1,27 +1,20 @@
 import heapq
 
-def add(G, heap, best, distance, x, y):
-    N = len(G)
-    if x < 0 or y < 0 or x >= N*5 or y >= N*5: return
-    cost = (G[x%N][y%N] + x//N + y//N - 1) % 9 + 1
-    if distance + cost >= best[x][y]: return
-    heapq.heappush(heap, (distance + cost, (x, y)))
-
 def dijkstra(G, start_node, end_node):
-    best = [[float('+Inf')]*len(G)*5 for _ in range(len(G)*5)]
+    N = len(G)
+    best = [[float('+Inf')]*N*5 for _ in range(N*5)]
     heap = []
+
     heapq.heappush(heap, (0, start_node))
     while len(heap):
         distance, (x, y) = heapq.heappop(heap)
-
-        if distance >= best[x][y]: continue
         if (x, y) == end_node: return distance
-        best[x][y] = distance
-        
-        add(G, heap, best, distance, x-1, y)
-        add(G, heap, best, distance, x+1, y)
-        add(G, heap, best, distance, x, y-1)
-        add(G, heap, best, distance, x, y+1)
+        for nx, ny in (x-1, y), (x+1, y), (x, y-1), (x, y+1):
+            if not (0 <= nx < N*5 and 0 <= ny < N*5): continue
+            ndistance = distance + (G[nx%N][ny%N] + nx//N + ny//N - 1) % 9 + 1
+            if ndistance >= best[nx][ny]: continue
+            best[nx][ny] = ndistance
+            heapq.heappush(heap, (ndistance, (nx, ny)))
 
 G = []
 while True:
