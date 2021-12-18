f7a6a836086ead7545a129ad1364ab41e5bea20e
Juan Lopes <juan.lopes@zwift.com>
2021-12-18 19:17:13 -0300

    Simplify

--- day18a.py
+++ day18a.py
@@ -9,16 +9,14 @@ def unwrap(tree, a=0, b=64):
     if tree[(a+b)//2] != None: return tree[(a+b)//2]
     return [unwrap(tree, a, (a+b)//2), unwrap(tree, (a+b)//2, b)]
 
-def scan_and_set(tree, indices, value):
-    for i in indices:
-        if tree[i] != None: tree[i] += value; break
-
 def explode(tree):
     for i in range(2, 64, 4):
         left, right = i - (i&-i)//2, i + (i&-i)//2
         if tree[i] == None and tree[left] != None and tree[right] != None:    
-            scan_and_set(tree, range(left-1, -1, -1), tree[left])
-            scan_and_set(tree, range(right+1, 64), tree[right])
+            for j in range(left-1, -1, -1):
+                if tree[j] != None: tree[j] += tree[left]; break
+            for j in range(right+1, 64):
+                if tree[j] != None: tree[j] += tree[right]; break
             tree[i], tree[left], tree[right] = 0, None, None
             return True
 
