555810e5466229b703a4a419f9c512374c7e36c4
Juan Lopes <juan.lopes@zwift.com>
2021-12-19 09:39:37 -0300

    Simplify

--- day18a.py
+++ day18a.py
@@ -12,19 +12,19 @@ def unwrap(tree, i):
 
 def explode(tree):
     for i in range(2, 64, 4):
-        if tree[i] == None and tree[L(i)] != None and tree[R(i)] != None:    
-            for j in range(L(i)-1, 0, -1):
-                if tree[j] != None: tree[j] += tree[L(i)]; break
-            for j in range(R(i)+1, 64):
-                if tree[j] != None: tree[j] += tree[R(i)]; break
-            tree[i], tree[L(i)], tree[R(i)] = 0, None, None
-            return True
+        if tree[L(i)] == None or tree[R(i)] == None: continue
+        for j in range(L(i)-1, 0, -1):
+            if tree[j] != None: tree[j] += tree[L(i)]; break
+        for j in range(R(i)+1, 64):
+            if tree[j] != None: tree[j] += tree[R(i)]; break
+        tree[i], tree[L(i)], tree[R(i)] = 0, None, None
+        return True
 
 def split(tree):
     for i in range(2, 64, 2):
-        if tree[i] != None and tree[i] >= 10:
-            tree[i], tree[L(i)], tree[R(i)] = None, tree[i]//2, (tree[i]+1)//2
-            return True
+        if tree[i] == None or tree[i] < 10: continue
+        tree[i], tree[L(i)], tree[R(i)] = None, tree[i]//2, (tree[i]+1)//2
+        return True
     
 def reduce(tree):
     tree = wrap(tree, 32, [None]*64)
