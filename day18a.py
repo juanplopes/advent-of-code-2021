75bb7e559c8b57eda0b3bd817d6fae636804754f
Juan Lopes <juan.lopes@zwift.com>
2021-12-18 23:39:32 -0300

    Simplify

--- day18a.py
+++ day18a.py
@@ -1,35 +1,35 @@
-def wrap(tree, a, b, nodes):
+def L(i): return i-(i&-i)//2
+def R(i): return i+(i&-i)//2
+def wrap(tree, i, nodes):
     if isinstance(tree, int): 
-        nodes[(a+b)//2] = tree
+        nodes[i] = tree
         return nodes
-    return wrap(tree[0], a, (a+b)//2, nodes) and wrap(tree[1], (a+b)//2, b, nodes)
+    return wrap(tree[0], L(i), nodes) and wrap(tree[1], R(i), nodes)
 
-def unwrap(tree, a=0, b=64):
-    if tree[(a+b)//2] != None: return tree[(a+b)//2]
-    return [unwrap(tree, a, (a+b)//2), unwrap(tree, (a+b)//2, b)]
+def unwrap(tree, i):
+    if tree[i] != None: return tree[i]
+    return [unwrap(tree, L(i)), unwrap(tree, R(i))]
 
 def explode(tree):
     for i in range(2, 64, 4):
-        left, right = i - (i&-i)//2, i + (i&-i)//2
-        if tree[i] == None and tree[left] != None and tree[right] != None:    
-            for j in range(left-1, 0, -1):
-                if tree[j] != None: tree[j] += tree[left]; break
-            for j in range(right+1, 64):
-                if tree[j] != None: tree[j] += tree[right]; break
-            tree[i], tree[left], tree[right] = 0, None, None
+        if tree[i] == None and tree[L(i)] != None and tree[R(i)] != None:    
+            for j in range(L(i)-1, 0, -1):
+                if tree[j] != None: tree[j] += tree[L(i)]; break
+            for j in range(R(i)+1, 64):
+                if tree[j] != None: tree[j] += tree[R(i)]; break
+            tree[i], tree[L(i)], tree[R(i)] = 0, None, None
             return True
 
 def split(tree):
     for i in range(2, 64, 2):
         if tree[i] != None and tree[i] >= 10:
-            left, right = i - (i&-i)//2, i + (i&-i)//2
-            tree[i], tree[left], tree[right] = None, tree[i] // 2, (tree[i]+1) // 2
+            tree[i], tree[L(i)], tree[R(i)] = None, tree[i]//2, (tree[i]+1)//2
             return True
     
 def reduce(tree):
-    tree = wrap(tree, 0, 64, [None]*64)
+    tree = wrap(tree, 32, [None]*64)
     while explode(tree) or split(tree): pass
-    return unwrap(tree)
+    return unwrap(tree, 32)
 
 def magnitude(tree):
     if isinstance(tree, int): return tree
