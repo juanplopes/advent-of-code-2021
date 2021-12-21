917f86f44a380348918afccb60ec33cdc5b0aace
Juan Lopes <juan.lopes@zwift.com>
2021-12-21 10:57:12 -0300

    Day 21

--- day21b.py
+++ day21b.py
@@ -1,23 +1,19 @@
 from collections import Counter
 from itertools import product
 
-p1, p2 = int(input().split(' ')[-1]), int(input().split(' ')[-1])
-T = Counter([(p1,p2,0,0)])
-
-answer1, answer2 = 0, 0
+T = Counter([(int(input().split(' ')[-1]),int(input().split(' ')[-1]),0,0)])
+A = [0, 0]
 for i in range(42):
-    T2 = Counter()
-    for (p1, p2, s1, s2), value in T.items():
-        if s1 == 21: answer1 += value
-        elif s2 == 21: answer2 += value
+    T, U = Counter(), T
+    for (p1, p2, s1, s2), value in U.items():
+        if s1 == 21 or s2 == 21: A[s2 == 21] += value
         elif i%2 == 0:
             for a, b, c in product((1,2,3), repeat=3):
                 np1 = (p1+a+b+c-1)%10+1
-                T2[(np1,p2,min(s1+np1, 21),s2)] += value
+                T[(np1,p2,min(s1+np1, 21),s2)] += value
         else:
             for a, b, c in product((1,2,3), repeat=3):
                 np2 = (p2+a+b+c-1)%10+1
-                T2[(p1,np2,s1,min(s2+np2, 21))] += value
-    T = T2    
+                T[(p1,np2,s1,min(s2+np2, 21))] += value
 
-print(max(answer1, answer2))
+print(max(A))
