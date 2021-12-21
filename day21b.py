20c59615bd4985f83d598fdd9ea1c9fe6e2a8435
Juan Lopes <juan.lopes@zwift.com>
2021-12-21 11:41:07 -0300

    Day 21

--- day21b.py
+++ day21b.py
@@ -1,19 +1,18 @@
 from collections import Counter
-from itertools import product
-
+D = [1, 3, 6, 7, 6, 3, 1]
 T = Counter([(int(input().split(' ')[-1]),int(input().split(' ')[-1]),0,0)])
 A = [0, 0]
 for i in range(42):
     T, U = Counter(), T
     for (p1, p2, s1, s2), value in U.items():
-        if s1 == 21 or s2 == 21: A[s2 == 21] += value
+        if s1 >= 21 or s2 >= 21: A[s2 >= 21] += value
         elif i%2 == 0:
-            for a, b, c in product((1,2,3), repeat=3):
-                np1 = (p1+a+b+c-1)%10+1
-                T[(np1,p2,min(s1+np1, 21),s2)] += value
+            for dice, freq in enumerate(D, 3):
+                np1 = (p1+dice-1)%10+1
+                T[(np1,p2,s1+np1,s2)] += freq*value
         else:
-            for a, b, c in product((1,2,3), repeat=3):
-                np2 = (p2+a+b+c-1)%10+1
-                T[(p1,np2,s1,min(s2+np2, 21))] += value
+            for dice, freq in enumerate(D, 3):
+                np2 = (p2+dice-1)%10+1
+                T[(p1,np2,s1,s2+np2)] += freq*value
 
 print(max(A))
