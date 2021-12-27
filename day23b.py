80c93f46a4e571ba1d488258ded0c31573452b23
Juan Lopes <juan.lopes@zwift.com>
2021-12-27 11:38:02 -0300

    Optimize

--- day23b.py
+++ day23b.py
@@ -1,9 +1,14 @@
 from collections import deque
 input(); input()
 
+B = [[sum(1 << j for j in range(a, b))+sum(1 << j for j in range(b+1, a+1)) 
+        for b in range(20)] 
+        for a in range(20)]
+
 states = []
 stacks = [deque() for _ in range(4)]
 best = [10000000000]
+out = [0]
 visited = {}
 IN, OUT, IN2 = 0, 1, 2
 
@@ -18,26 +23,30 @@ def backtrack(cost, level=0):
         return
 
     for i, (a, s, b, q) in enumerate(states):
-        if level <= 2: print(' ' * level, i, best, len(visited))
+        if level < 2: print(' ' * level, i, best, len(visited))
         if s == IN and stacks[b][-1] == i and any(states[x][0] != states[x][2] for x in stacks[b]):
             for dst in range(11):
                 if dst in (2, 4, 6, 8): continue
                 src = b*2+2
-                if any(dst <= b2 < src or src < b2 <= dst for a2, s2, b2, _ in states if s2 == OUT): 
+                if B[dst][src] & out[0]: 
                     continue
                 stacks[b].pop()
                 states[i] = (a, OUT, dst, 0)
+                out[0] |= 1 << dst
                 backtrack(cost + 10**a * (abs(src-dst) + 4-len(stacks[b])), level+1)
                 states[i] = (a, s, b, q)
                 stacks[b].append(i)
+                out[0] &= ~(1 << dst)
         elif s == OUT and len(stacks[a]) < 4 and all(states[x][0] == states[x][2] for x in stacks[a]):
             dst = a*2+2
-            if not any(dst <= b2 < b or b < b2 <= dst for a2, s2, b2, _ in states if s2 == OUT): 
+            if not (B[dst][b] & out[0]):
                 stacks[a].append(i)
                 states[i] = (a, IN2, a, len(stacks[a]))
+                out[0] &= ~(1 << b)
                 backtrack(cost + 10**a * (abs(b-dst) + 5-len(stacks[a])), level+1)
                 states[i] = (a, s, b, q)
                 stacks[a].pop()
+                out[0] |= 1 << b
 
 for i, x in enumerate(input().replace('###', '').split('#')):
     stacks[i].appendleft(len(states))
